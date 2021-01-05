87d33742dc8622608aa5ef177a290274bb15194d
Amadeus <acastillo@auronix.com>
2021-01-04 20:27:35 -0600

    first load

---
+++ TestBox/TestBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
