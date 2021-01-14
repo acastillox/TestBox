531dea9a7ae8275a88a25984efa7e2d5e945a7d2
Amadeus <acastillo@auronix.com>
2021-01-13 19:59:37 -0600

    minver

--- TestBox/TestBox/Program.cs
+++ TestBox/TestBox/Program.cs
@@ -7,6 +7,7 @@ namespace TestBox
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("No ThisAssembly class :(");
         }
     }
 }
