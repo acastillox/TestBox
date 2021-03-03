13382578b54872d49aa60049e03f434aee76f245
Amadeus <acastillo@auronix.com>
2021-03-03 17:49:22 -0600

    sadfasdf

--- TestBox/TestBox/Program.cs
+++ TestBox/TestBox/Program.cs
@@ -10,6 +10,8 @@ namespace TestBox
             Console.WriteLine($"Current Version: {ThisAssembly.Version}");
             Console.WriteLine($"Current Version: {ThisAssembly.SimpleVersion}");
             Console.WriteLine($"Current Version: {ThisAssembly.InformationalVersion}");
+			
+			// asdadsfadsf
         }
     }
 }
